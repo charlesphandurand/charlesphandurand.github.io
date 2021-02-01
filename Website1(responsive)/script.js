aebeca89fc8fdf48d87fce2a146f878856283e5e
screlyin <71703841+lsajui@users.noreply.github.com>
2021-02-01 16:57:06 +0800

    trying to upload my trash
    hope this can be my material for study

---
+++ Website1(responsive)/script.js
@@ -0,0 +1,13 @@
+$('html, body, .page-scroll').on('click', function(e) {
+
+    // var tujuan = $(this).attr('href');
+   
+    // var elemenTujuan = $(tujuan);
+   
+    // $('html , body').animate({
+    //  scrollTop: elemenTujuan.offset().top - 50
+    // });
+   
+    // e.preventDefault();
+    console.log('ok');
+   });
